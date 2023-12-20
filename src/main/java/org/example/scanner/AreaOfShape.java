aa3c77053418df643d551e8d9168b7ac0e86a1ae
Shweta Gokhale <shwetagokhale123@gmail.com>
2023-12-20 15:20:04 +0530

    Assignment Of Scanner - Code Of Area Of Shape

--- src/main/java/org/example/scanner/AreaOfShape.java
+++ src/main/java/org/example/scanner/AreaOfShape.java
@@ -3,18 +3,41 @@ package org.example.scanner;
 import java.sql.SQLOutput;
 import java.util.Scanner;
 
+// Area of Shapes:
+//Calculate the area of different shapes (circle, square, triangle) based on user input. Use appropriate formulas for each shape.
+
+
+
 public class AreaOfShape
 {
+    static float pi = 3.14f;
     public static void main(String[] args)
     {
-        int a;
+       //For Square
+
         Scanner obj = new Scanner(System.in);
-        System.out.println("Enter no = ");
-        int b = obj.nextInt();
-        a = b * b;
+        System.out.println("Enter Side = ");
+        int side = obj.nextInt();
+        int area  = side * side;
+
+        System.out.println("Area Of Square = "+area);
 
 
+        //For Circle
+        System.out.println("Enter Radius = ");
+        float radius = obj.nextFloat();  //float radius = declaration
+        double cir = 3.14 * radius * radius;
+        System.out.println("Area of Circle = " +cir);
 
-        System.out.println("Area Of Square = "+a);
+        //For Triangle
+        System.out.println("Enter Base  = ");
+        int base = obj.nextInt();
+
+        System.out.println("Enter Height  = ");
+        int height = obj.nextInt();
+        int area1 = (base * height)/2;
+
+        System.out.println("Area Of Triangle = " +area1);
     }
 }
+
