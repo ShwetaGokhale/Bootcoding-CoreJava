0af1dc993be9f000604a527c0331548186a9b299
Shweta Gokhale <shwetagokhale123@gmail.com>
2023-12-22 01:36:07 +0530

    Method Assignment - Code Of Addition

---
+++ src/main/java/org/example/methods/Addition.java
@@ -0,0 +1,16 @@
+package org.example.methods;
+
+public class Addition
+{
+    public int add(int x , int y)
+    {
+        int sum = x + y;
+        return sum;
+    }
+    public static void main(String[] args)
+    {
+        Addition a = new Addition();
+        int r = a.add(30 , 10);
+        System.out.println(r);
+    }
+}
