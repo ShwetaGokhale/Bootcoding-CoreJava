5616312395110c2e64dd47322bab0d4ccaf49cc8
Shweta Gokhale <shwetagokhale123@gmail.com>
2023-12-21 11:42:11 +0530

    Assignment Of Scanner - Code Of Sum Of Digits

---
+++ src/main/java/org/example/methods/Calculate.java
@@ -0,0 +1,20 @@
+package org.example.methods;
+
+public class Calculate
+{
+    public int add(int a , int b)
+    {
+        int sum = a + b;
+        return sum;
+    }
+
+
+    public static void main(String args[])
+    {
+        Calculate obj = new Calculate();
+        int res = obj.add(10, 20);
+        System.out.println(res);
+
+
+    }
+}
