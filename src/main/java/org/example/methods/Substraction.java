b1aa4a11acb0a1e179d950ff9cefb68f7a2a85b4
Shweta Gokhale <shwetagokhale123@gmail.com>
2023-12-24 17:45:50 +0530

    Method Assignment - Code Of Substraction

---
+++ src/main/java/org/example/methods/Substraction.java
@@ -0,0 +1,18 @@
+package org.example.methods;
+
+public class Substraction
+{
+    public int sub(int i , int j)
+    {
+        int sum = i - j;
+        System.out.println("Given Numbers = " +i+"  "+j);
+        return sum;
+    }
+    public static void main(String[] args)
+    {
+        Substraction s = new Substraction();
+        int value = s.sub(30 , 10);
+        System.out.println("Substraction = " +value);
+
+    }
+}
