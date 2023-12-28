460c73dbbd89c951919a5d1f9c23ce07f16293d0
Shweta Gokhale <shwetagokhale123@gmail.com>
2023-12-28 22:46:33 +0530

    Variable Assignment - Code Of FinalVariable

--- src/main/java/org/example/variables/final_variable/FinalVariable.java
+++ src/main/java/org/example/variables/final_variable/FinalVariable.java
@@ -7,7 +7,7 @@ class  FinalVariable
 		
 		final  float  PI  =  3.142f;
 		final  int  MAX_SIZE  =  12;
-		//PI  =  3.223f;
+		// PI  =  3.223f;
 		//MAX_SIZE  =  13;
 	                  System.out.println("PI  =  " +PI);
 		System.out.println("MAX SIZE   =  " +MAX_SIZE);
