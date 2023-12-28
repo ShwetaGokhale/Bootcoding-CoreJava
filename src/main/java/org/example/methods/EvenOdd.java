6c1dbd8fa1dbebd5e9714b2b13f994d489358bb9
Shweta Gokhale <shwetagokhale123@gmail.com>
2023-12-28 22:43:58 +0530

    Method Assignment - Code Of EvenOdd

---
+++ src/main/java/org/example/methods/EvenOdd.java
@@ -0,0 +1,44 @@
+package org.example.methods;
+
+import java.util.EventObject;
+
+public class EvenOdd
+{
+//    public int number(int i)
+//    {
+//        System.out.println("Given no = " +i);
+//        if(i % 2 == 0)
+//        {
+//            System.out.println("Given number is even");
+//        }
+//        else
+//        {
+//            System.out.println("Given number is odd");
+//        }
+//        return i;
+//    }
+//    public static void main(String[] args)
+//    {
+//        EvenOdd e = new EvenOdd();
+//        int a = e.number(6);
+//    }
+        public  void number(int i)
+        {
+            System.out.println("Given no = " +i);
+            if(i % 2 == 0)
+        {
+            System.out.println("Given number is even");
+        }
+        else
+        {
+            System.out.println("Given number is odd");
+        }
+        }
+
+    public static void main(String[] args)
+    {
+        EvenOdd e = new EvenOdd();
+        e.number(5);
+
+    }
+}
