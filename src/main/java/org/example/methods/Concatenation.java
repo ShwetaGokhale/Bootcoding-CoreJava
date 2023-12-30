e6c31484d7060e1f83756ac7736a8004f0c6c0b0
Shweta Gokhale <shwetagokhale123@gmail.com>
2023-12-30 23:28:09 +0530

    Assignment Method - Code Of Concatenation

--- src/main/java/org/example/methods/Concatenation.java
+++ src/main/java/org/example/methods/Concatenation.java
@@ -1,16 +1,29 @@
 package org.example.methods;
 
+import java.util.concurrent.Callable;
+
 public class Concatenation
 {
-    public String concate(String s1 , String s2)
+//    public String concate(String s1 , String s2)
+//    {
+//        System.out.println("String = " +s1 +"   " +s2);
+//        return s1 + s2;
+//    }
+//    public static void main(String[] args)
+//    {
+//        Concatenation s = new Concatenation();
+//        String add = s.concate("Bootcoding" , "Classes");
+//        System.out.println("Concate = " +add);
+//    }
+    public void concate(String a , String b)
     {
-        System.out.println("String = " +s1 +"   " +s2);
-        return s1 + s2;
+        System.out.println("Given String = " + a +"  "+b);
+        System.out.println(a + b);
     }
     public static void main(String[] args)
     {
-        Concatenation s = new Concatenation();
-        String add = s.concate("Bootcoding" , "Classes");
-        System.out.println("Concate = " +add);
+        Concatenation c1  = new Concatenation();
+        c1.concate("Bootcoding" , "Coaching");
+
     }
 }
