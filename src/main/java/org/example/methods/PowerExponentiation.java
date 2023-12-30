70585f6a4a3d03a641332f381b0b54b598e6c9c6
Shweta Gokhale <shwetagokhale123@gmail.com>
2023-12-30 23:30:55 +0530

    Assignment Method - Code Of PowerExponentiation

---
+++ src/main/java/org/example/methods/PowerExponentiation.java
@@ -0,0 +1,23 @@
+package org.example.methods;
+
+public class PowerExponentiation
+{
+    public int power(int i)
+    {
+        int b = 2;
+        int power = 1;
+        System.out.println("The 2 raised to the power of number " + (0) + " is " + (power));
+        for(int j = 1; j <= i; j++)
+        {
+            power = power * b;
+            System.out.println("The 2 raised to the power of number " + (j) + " is " + (power));
+        }
+        return power;
+    }
+    public static void main(String[] args)
+    {
+        PowerExponentiation p = new PowerExponentiation();
+        int c = p.power(5);
+        System.out.println("The Power is .... " +c);
+    }
+}
