0d61e81813ed5f7abc3c0866ac1fc3b20741306c
Shweta Gokhale <shwetagokhale123@gmail.com>
2023-12-30 23:29:50 +0530

    Assignment Method - Code Of Multiplication

---
+++ src/main/java/org/example/methods/Multiplication.java
@@ -0,0 +1,18 @@
+package org.example.methods;
+
+public class Multiplication
+{
+    public int multiply(int a , int b)
+    {
+        int c = a * b;
+        System.out.println("Given Numbers = " +a+"  "+b);
+        return c;
+    }
+    public static void main(String[] args)
+    {
+        Multiplication m = new Multiplication();
+        int  value = m.multiply(3 , 2);
+        System.out.println("Multiplication = " +value);
+
+    }
+}
