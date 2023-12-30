d69dbdff6f2aac1875a31d4e317ade2cc59a8e1e
Shweta Gokhale <shwetagokhale123@gmail.com>
2023-12-30 23:29:06 +0530

    Assignment Method - Code Of Factorial

---
+++ src/main/java/org/example/methods/Factorial.java
@@ -0,0 +1,22 @@
+package org.example.methods;
+
+public class Factorial
+{
+    public int factorial(int i)
+    {
+        System.out.println("Given no = "+i);
+        int fact = 1;
+        for(int j = 1; j <= i; j++)
+        {
+            fact = fact * j;
+        }
+        return fact;
+    }
+    public static void main(String[] args)
+    {
+        Factorial f = new Factorial();
+        int s = f.factorial(5);
+        System.out.println("Factorial of given no = "+s);
+
+    }
+}
